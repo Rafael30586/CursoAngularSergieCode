96d38fd76cbbde538fcc41e18479d4b7afa717cc
Rafael305 <fraq86@gmail.com>
2025-07-14 18:48:47 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PadreComponent } from './padre/padre.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //Acá van componentes
+    AppComponent, PadreComponent
   ],
-  imports: [
+  imports: [ // Acá van algunos módulos
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [], // Acá pueden ir o no componentes dependiendo de si está conectado con otros
   bootstrap: [AppComponent]
 })
 export class AppModule { }
